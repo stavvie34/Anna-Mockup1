86ad0fc90a201e94a40bd182e689d09c786a68e5
stavvie34 <alexanderlee58col@gmail.com>
2017-03-12 16:59:43 -0700

    trying to make it so when you click link in nav, active is removed and added

--- js/main.js
+++ js/main.js
@@ -190,6 +190,40 @@
 
 	};
 
+	// Reflect scrolling in navigation
+	var navActive = function(section) {
+
+		var $el = $('#navbar > ul');
+		$el.find('li').removeClass('active');
+		$el.each(function(){
+			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+		});
+
+	};
+
+	var navigationSection = function() {
+
+		var $section = $('section[data-section]');
+
+		$section.waypoint(function(direction) {
+
+		  	if (direction === 'down') {
+		    	navActive($(this.element).data('section'));
+		  	}
+		}, {
+	  		offset: '150px'
+		});
+
+		$section.waypoint(function(direction) {
+		  	if (direction === 'up') {
+		    	navActive($(this.element).data('section'));
+		  	}
+		}, {
+		  	offset: function() { return -$(this.element).height() + 155; }
+		});
+
+	};
+
 	// Document on load.
 	$(function(){
 		mainMenu();
@@ -201,6 +235,7 @@
 
 		windowScroll();
 		burgerMenu();
+		navigationSection();
 	});
 
 
